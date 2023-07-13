498bc26ceb8cb9845002d0128c6651dd9cf57f8f
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-07-13 10:26:32 +0400

    sphinx stuff

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-if [ -z "$IMAGE" ]; then
-    IMAGE=backprop_tools_docs
-fi
 docker run -it --rm --platform linux/amd64 -p 8888\:8888 \
 -v $(pwd)/../include/backprop_tools/\:/usr/local/include/backprop_tools/\:ro \
 -v $(pwd):/workspace/\:rw -w /workspace \
-$IMAGE \
+backprop_tools_docs \
 "$@"
