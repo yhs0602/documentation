3d80866a1b68922f5a3213f01ffdcce3143f09ee
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-11-20 15:20:42 -0500

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run -it --rm --platform linux/amd64 -p 8888\:8888 \
 -v $(pwd)/../include/rl_tools/\:/usr/local/include/rl_tools/\:ro \
 -v $(pwd):/workspace/\:rw -w /workspace \
-rl_tools_docs \
+$IMAGE \
 "$@"
