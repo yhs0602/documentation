b40ae63426dc53e1e3951b756770c87600016084
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-04-12 13:13:31 -0400

    bumping static; not remapping user on macos

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+if [ $(uname -s) = "Linux" ]; then
+  USER_OPTION="--user $(id -u):$(id -g)"
+fi
 docker run -it --rm --platform linux/amd64 -p 8888\:8888 \
---user $(id -u):$(id -g) \
+$USER_OPTION \
 -v $(pwd)/../include/rl_tools/\:/usr/local/include/rl_tools/\:ro \
 -v $(pwd):/workspace/\:rw -w /workspace \
 $IMAGE \
